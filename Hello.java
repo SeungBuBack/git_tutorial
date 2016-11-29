053e188990700c74ffadfead36689d6f0a33fe35
Seungbu Back <57backsb@gmail.com>
2016-11-29 11:36:57 +0900

    add

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("hellow자바");
 		System.out.println("Hello her Git");
-		System.out.println("Hello his Git");
+		System.out.println("Hello his Git");;
 	}
 }
